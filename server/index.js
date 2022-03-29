5d3236b60e083c5287c124070200d418d41264d7
singh08prashant <singh08prashant@gmail.com>
2022-03-29 23:32:15 +0530

    implemented auth

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
