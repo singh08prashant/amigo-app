57f594ba624b6a7f9e6836d796accecf41a2f07b
singh08prashant <singh08prashant@gmail.com>
2022-03-30 18:16:40 +0530

    amigo client

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+
 import { AddChannel } from '../assets';
 
-const TeamChannelList = ({ children, error = false, loading, type }) => {
-
+const TeamChannelList = ({ setToggleContainer, children, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
     if(error) {
         return type === 'team' ? (
-            <div className='team-channel-list'>
-                <p className='team-channel-list__message'>
-                    Connection error, please try again!
+            <div className="team-channel-list">
+                <p className="team-channel-list__message">
+                    Connection error, please wait a moment and try again.
                 </p>
             </div>
         ) : null
@@ -15,25 +15,32 @@ const TeamChannelList = ({ children, error = false, loading, type }) => {
 
     if(loading) {
         return (
-            <div className='team-channel-list'>
-                <p className='team-channel-list__message loading'>
+            <div className="team-channel-list">
+                <p className="team-channel-list__message loading">
                     {type === 'team' ? 'Channels' : 'Messages'} loading...
                 </p>
             </div>
         )
     }
 
-  return (
-    <div className='team-channel-list'>
-        <div className='team-channel-list__header'>
-            <p className='team-channel-list__header__title'>
-            {type === 'team' ? 'Channels' : 'Direct Messages'}
-            </p>
-            {/* Button - add channel */}
+    return (
+        <div className="team-channel-list">
+            <div className="team-channel-list__header">
+                <p className="team-channel-list__header__title">
+                    {type === 'team' ? 'Channels' : 'Direct Messages'}
+                </p>
+                <AddChannel 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType} 
+                    setIsEditing={setIsEditing}
+                    type={type === 'team' ? 'team' : 'messaging'}
+                    setToggleContainer={setToggleContainer}
+                />
+            </div>
+            {children}
         </div>
-        {children}
-    </div>
-  )
+    )
 }
 
 export default TeamChannelList
